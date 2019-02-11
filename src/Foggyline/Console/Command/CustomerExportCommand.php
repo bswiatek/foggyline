c26e51b7652982115bbe0d7506130224fe138d97
Bartłomiej Świątek <emini1@swiatek.biz>
2019-02-11 21:40:42 +0100

    Customer default commands

---
+++ src/Foggyline/Console/Command/CustomerExportCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Foggyline\Console\Command;
+
+use Symfony\Component\Console\{
+    Command\Command,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+
+class CustomerExportCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('customer:export')->setDescription('Exports one or more customers.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Some logic here
+        $output->writeln('Customers exported.');
+    }
+}
---
+++ src/Foggyline/Console/Command/CustomerRegisterCommand.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Foggyline\Console\Command;
+
+use Symfony\Component\Console\{
+    Command\Command,
+    Input\InputInterface,
+    Output\OutputInterface
+};
+
+class CustomerRegisterCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('customer:register')->setDescription('Registers new customer.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // Some logic here
+        $output->writeln('Customer registered.');
+    }
+}
