02a5abd59d6ad76c69236f02c0f3bd29a61ac4d2
Bartłomiej Świątek <emini1@swiatek.biz>
2019-02-15 20:52:53 +0100

    input arguments

--- src/Foggyline/Console/Command/CustomerRegisterCommand.php
+++ src/Foggyline/Console/Command/CustomerRegisterCommand.php
@@ -2,22 +2,32 @@
 
 namespace Foggyline\Console\Command;
 
-use Symfony\Component\Console\{
-    Command\Command,
+use Symfony\Component\Console\{Command\Command,
+    Input\InputArgument,
     Input\InputInterface,
-    Output\OutputInterface
-};
+    Input\InputOption,
+    Output\OutputInterface};
 
 class CustomerRegisterCommand extends Command
 {
     protected function configure()
     {
-        $this->setName('customer:register')->setDescription('Registers new customer.');
+        $this->setName('customer:register')
+            ->addArgument('name', InputArgument::REQUIRED, 'Customer full name.')
+            ->addArgument('email', InputArgument::REQUIRED, 'Customer email address.')
+            ->addArgument('dob', InputArgument::OPTIONAL, 'Customer date of birth.')
+            ->addOption('email', null, InputOption::VALUE_REQUIRED, 'Send email to customer?')
+            ->addOption('log',  null, InputOption::VALUE_OPTIONAL, 'Log to event system?')
+            ->setDescription('Registers new customer.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         // Some logic here
-        $output->writeln('Customer registered.');
+        var_dump($input->getArgument('name'));
+        var_dump($input->getArgument('dob'));
+        var_dump($input->getArgument('email'));
+        var_dump($input->getOption('email'));
+        var_dump($input->getOption('log'));
     }
 }
