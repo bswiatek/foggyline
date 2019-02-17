46f8adb86bd3c9d136caa8d8cc55229ece837cba
Bartłomiej Świątek <emini1@swiatek.biz>
2019-02-17 11:28:49 +0100

    helpers - process and table

--- src/Foggyline/Console/Command/CustomerExportCommand.php
+++ src/Foggyline/Console/Command/CustomerExportCommand.php
@@ -2,11 +2,11 @@
 
 namespace Foggyline\Console\Command;
 
-use Symfony\Component\Console\{
-    Command\Command,
+use Symfony\Component\Console\{Command\Command,
+    Helper\ProgressBar,
+    Helper\Table,
     Input\InputInterface,
-    Output\OutputInterface
-};
+    Output\OutputInterface};
 
 class CustomerExportCommand extends Command
 {
@@ -17,7 +17,28 @@ class CustomerExportCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Some logic here
+        $customers = [
+            ['John Doe', 'john.doe@mail.loc', '1983-01-16'],
+            ['Samantha Smith', 'samantha.smith@mail.loc', '1986-01-16'],
+            ['Robert Black', 'robert.blac@mail.loc', '1989-01-16'],
+        ];
+
+        $progress = new ProgressBar($output, count($customers));
+        $progress->start();
+
+        for($i = 1; $i <= count($customers); $i++) {
+            sleep(3);
+            $progress->advance();
+        }
+
+        $progress->finish();
+
+        $table = new Table($output);
+        $table->setHeaders(['Name', 'Email', 'DOB'])
+            ->setRows($customers)
+            ->render();
+
+
         $output->writeln('Customers exported.');
     }
 }
